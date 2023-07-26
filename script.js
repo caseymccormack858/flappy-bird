22cef4d5096ed0a3c1d8428dbc4d031548eb2b53
Casey McCormack <caseymccormack858@gmail.com>
2023-07-25 23:25:40 -0700

    //context changed

--- script.js
+++ script.js
@@ -26,14 +26,14 @@ window.onload = function() {
     context = board.getContext("2d"); //used for drawing on the board
 
     // draw flappy bird // change to green //rectangle 
-    context.fillStyle ="green";
-    context.fillRect(bird.x, bird.y, bird.width, bird.height);
+    // context.fillStyle ="green";
+   // context.fillRect(bird.x, bird.y, bird.width, bird.height);
 
 
 //load image //
 birdImg = new Image();
 birdImg.src = "./flappybird.png";
-birdImg.onload = function(){
+birdImg.onload = function() {
 context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
 }
 }
